c2d2b4bc9588b20b19b2e696843c94823e4c8451
Kuan <huangkuanlin@gmail.com>
2016-07-08 01:09:28 -0500

    Improved documentation and organization

--- outlier.R
+++ outlier.R
@@ -21,9 +21,6 @@ figurePath=paste("figures/", date, "/",sep="")
 system(paste("mkdir ", figurePath, sep=""))
 pd = paste(figurePath, date, "_premed", sep="")
 
-druggable_f = read.table(header=FALSE, stringsAsFactors = F, file = "genes_for_highExpression_analysis_20160128.txt")
-druggable = as.vector(t(druggable_f))
-
 ##### functions #####
 unfactorize = function(df){
   for(i in which(sapply(df, class) == "factor")) df[[i]] = as.numeric(as.character(df[[i]]))
@@ -53,12 +50,14 @@ find_outlier = function(m, name="dataset", barplot = TRUE, plot=TRUE, printOrder
   for (i in 1:nrow(m2)){
     # modified z-score for outlier: Boris Iglewicz and David Hoaglin (1993), "Volume 16: How to Detect and Handle Outliers", The ASQC Basic References in Quality Control: Statistical Techniques, Edward F. Mykytka, Ph.D., Editor.
     # outlier_mzscore[i,]  = 0.6745*(m2[i,]-median(m2[i,], na.rm=TRUE))/mad(m2[i,], na.rm=TRUE)
+    
     # box-plot definition of outlier
     IQR = quantile(m2[i,], probs=0.75, na.rm=T) - quantile(m2[i,], probs=0.25, na.rm=T) 
     outlier_box[i,] = (m2[i,] >= quantile(m2[i,], probs=0.75, na.rm=T) + 1.5*IQR)
     # outlier_box2[i,] = (m2[i,] >= quantile(m2[i,], probs=0.75, na.rm=T) + 3.5*IQR) #outer fences
     outlier_mzscore[i,] = (m2[i,] - quantile(m2[i,], probs=0.75, na.rm=T))/IQR
   }
+  
   # output the outlier score table
   fn = paste(pd,name,'outlier_score_table.txt', sep="_")
   write.table(outlier_mzscore, file=fn, quote=F, row.names=T, sep="\t", col.names=NA)
@@ -74,11 +73,10 @@ find_outlier = function(m, name="dataset", barplot = TRUE, plot=TRUE, printOrder
   results$freq = results$count/results$cohort_size
   results$marker = row.names(results)
   
+  # plotting bar plots indicating number of samples having the gene outlier
   if (barplot){
     fn = paste(pd, name, '_outlier_counts.pdf',sep ="_")
     results_sum = results[c(1:10),,drop=F]
-    #colnames(results_sum)[1] = "count"
-    #results_sum$marker = as.factor(row.names(results_sum))
     results_sum$marker = factor(results_sum$marker, levels = results_sum$marker[order(results_sum$count, decreasing=T)])
     
     p = ggplot(data=results_sum, aes(x=marker, y=count))
@@ -106,6 +104,7 @@ find_outlier = function(m, name="dataset", barplot = TRUE, plot=TRUE, printOrder
   colnames(top_outlier)=colnames(top_outlier_zscore)
   colnames(top_outlier_boolean)=colnames(top_outlier_zscore)
   colnames(top_outlier_raw)= colnames(top_outlier_zscore)
+  
   # rank order based on zscore
   for (i in colnames(zscore)){
     whim=zscore[,i]
@@ -118,6 +117,7 @@ find_outlier = function(m, name="dataset", barplot = TRUE, plot=TRUE, printOrder
     top_outlier[i,] = names(a)
   }
   
+  # whether the ordered outlier tables will be saved as additional txt files
   if (printOrderTables){
     a=rbind(top_outlier, top_outlier_zscore)
     a = a[order(row.names(a)),]
@@ -136,36 +136,12 @@ find_outlier = function(m, name="dataset", barplot = TRUE, plot=TRUE, printOrder
   }
   
   ##### plotting #####
-  # how many outliers should be shown?
+  # determine how many outliers should be shown
   num_shown=1
   for (i in 1:nrow(top_outlier_boolean)){
     row_outlier = sum(top_outlier_boolean[i,], na.rm=T)
     if (row_outlier > num_shown) {num_shown = row_outlier}
   }
-  
-  # plot only the outliers: has to do this through command line because of not specifying "data" in ggplot2
-  if (FALSE){ 
-    #num_shown = 3
-    if (num_shown > 5) {num_shown=5}
-    # plot
-    top_outlier.m <- melt(top_outlier[,c(1:num_shown)])
-    top_outlier_zscore.m <- melt(top_outlier_zscore[,c(1:num_shown)])
-    top_outlier_boolean.m <- melt(top_outlier_boolean[,c(1:num_shown)])
-    
-    fn = paste(pd, name, 'top_outlier_only_score.pdf',sep ="_")
-    YlOrRd = brewer.pal(9, "YlOrRd") 
-    getPalette = colorRampPalette(YlOrRd)
-    outlier.colors=c("NA", "#000000")
-    top_outlier.m$value = as.character(top_outlier.m$value)
-    p = ggplot()
-    p = p + geom_tile(aes(x=as.factor(top_outlier_zscore.m$Var1), y=top_outlier_zscore.m$Var2, fill=ifelse(top_outlier_boolean.m$value,top_outlier_zscore.m$value,NA)), linetype="blank") + scale_fill_gradientn(name= "Outlier score", colours=getPalette(100), na.value=NA, limits=c(0,6))
-    p = p + geom_tile(data=top_outlier_boolean.m, aes(x=as.factor(Var1), y=Var2, color=value), fill=NA, size=0.5) + scale_colour_manual(name="Outlier",values = outlier.colors)
-    p = p + geom_text(aes(x=top_outlier.m$Var1, y=top_outlier.m$Var2, label = ifelse(top_outlier_boolean.m$value,top_outlier.m$value,NA), stringsAsFactors=FALSE), color="black", size=7, angle=90)
-    p = p + xlab("Sample") + ylab(paste("Top", name,"outliers", sep=" ")) + theme_bw() + 
-      theme(axis.title = element_text(size=16), axis.text.x = element_text(angle = 90, vjust = 0.5, colour="black", size=14), axis.text.y = element_blank(),axis.ticks.y = element_blank())#element_text(colour="black", size=14))
-    p
-    ggsave(file=fn, height=8, width=w, useDingbats=FALSE)   
-  }  
   
   # version that plotted everything
   if (plot){
@@ -178,8 +154,6 @@ find_outlier = function(m, name="dataset", barplot = TRUE, plot=TRUE, printOrder
     colnames(top_outlier_boolean.m)=c("Var1","Var2","value")
     
     fn = paste(pd, name, 'top_outlier_score_all.pdf',sep ="_")
-    #     YlGnBu = brewer.pal(9, "YlGnBu") 
-    #     getPalette = colorRampPalette(YlGnBu)
     YlOrRd = brewer.pal(9, "YlOrRd") 
     getPalette = colorRampPalette(YlOrRd)
     outlier.colors=c("NA", "#000000")
@@ -201,12 +175,17 @@ find_outlier = function(m, name="dataset", barplot = TRUE, plot=TRUE, printOrder
 }
 
 ##### MAIN CODE: demonstrate usage #####
-#if (FALSE){ 
+if (FALSE){ 
+  # read in a gene list
+  druggable_f = read.table(header=FALSE, stringsAsFactors = F, file = "genes_for_highExpression_analysis_20160128.txt")
+  druggable = as.vector(t(druggable_f))
+  
   # expect a gene-sample (row-column) dataset
   expTab = read.table(header=TRUE, sep="\t", file='/Users/khuang/Box\ Sync/PhD/collaborations/premed_2015/data/All_gene_RNASeq/raw_output/BRCA_RSEM_hugo.txt')
   names = make.names(expTab[,1], unique =T)
   row.names(expTab) = names
+  
   expTab.d = expTab[names %in% druggable,-c(1,2)] # extract only the druggable genes
-  expTab.d = log2(unfactorize(expTab.d)+1)
+  expTab.d = log2(unfactorize(expTab.d)+1) # log2 transofrm
   expTab_druggable = find_outlier(expTab.d, name = "druggable exp") # find outlier
-#}
+}
